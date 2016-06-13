41488c13993d312fb3f62d54a65506ace57eff01
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-06-13 19:52:28 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# go-util
+
+Personal collection of various util functions. See individual folders for specifics.
+
+## Usage
+
+See [documentation](https://godoc.org/github.com/frozzare/go-util).
---
+++ httputil/json.go
@@ -0,0 +1,28 @@
+package httputil
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// GetJSON will bind response body from a url to a struct or return a error.
+func GetJSON(v interface{}, target interface{}) error {
+	config, err := getConfig(v)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("GET", config.URL, nil)
+	if err != nil {
+		return err
+	}
+
+	res, err := config.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	return json.NewDecoder(res.Body).Decode(target)
+}
