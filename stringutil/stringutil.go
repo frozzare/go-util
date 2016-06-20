068887ebdf8e2d9ff9539055478e74da589306c1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-06-20 08:36:08 +0200

    Add more docs to truncate function

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -69,6 +69,8 @@ func Reverse(s string) string {
 }
 
 // Truncate will truncate the string and add dots or given character at the end.
+// The additional values are length of the dots or given character and the character
+// that should be used.
 func Truncate(s string, w int, args ...interface{}) string {
 	c := "."
 	l := 3
