b83a8107884b961bca20d5e881a9fa12511e9241
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-11-15 08:24:28 +0100

    Add cusotm default transport

--- httputil/client.go
+++ httputil/client.go
@@ -1,6 +1,10 @@
 package httputil
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+	"time"
+)
 
 // Client represents the custom http client.
 type Client struct {
@@ -11,6 +15,24 @@ type Client struct {
 func NewClient(client *http.Client) *Client {
 	if client == nil {
 		client = http.DefaultClient
+	}
+
+	// Set a custom default transport instead of the default transport.
+	// https://golang.org/src/net/http/transport.go#L33
+	if client.Transport == nil {
+		client.Transport = &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ResponseHeaderTimeout: 10 * time.Second,
+			ExpectContinueTimeout: 10 * time.Second,
+			IdleConnTimeout:       60 * time.Second,
+			MaxIdleConns:          5,
+			MaxIdleConnsPerHost:   5,
+			DisableKeepAlives:     false,
+		}
 	}
 
 	return &Client{client}
