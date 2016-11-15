c1baa1d60d0ee3a7eb159f9a548cbfb0ff4124e0
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-11-15 08:04:13 +0100

    Modify NewClient to take http.Client instead of custom config

--- httputil/client.go
+++ httputil/client.go
@@ -1,40 +1,17 @@
 package httputil
 
-import (
-	"crypto/tls"
-	"net"
-	"net/http"
-	"time"
-)
+import "net/http"
 
 // Client represents the custom http client.
 type Client struct {
 	*http.Client
 }
 
-// Config represents client config.
-type Config struct {
-	DualStack  bool
-	SkipVerify bool
-	Timeout    time.Duration
-}
-
 // NewClient will create a new http client instance.
-func NewClient(config *Config) *Client {
-	if config == nil {
-		config = &Config{}
+func NewClient(client *http.Client) *Client {
+	if client == nil {
+		client = http.DefaultClient
 	}
 
-	t := &http.Transport{
-		Dial: (&net.Dialer{
-			DualStack: config.DualStack,
-			Timeout:   config.Timeout,
-		}).Dial,
-	}
-
-	if config.SkipVerify {
-		t.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	}
-
-	return &Client{&http.Client{Transport: t}}
+	return &Client{client}
 }
