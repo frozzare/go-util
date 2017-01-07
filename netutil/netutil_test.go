c19cb7132a1248b7fece23cae14398177e3fc919
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-01-07 16:52:31 +0100

    Add netutil

---
+++ netutil/netutil_test.go
@@ -0,0 +1,12 @@
+package netutil
+
+import (
+	"testing"
+	"github.com/frozzare/go-assert"
+)
+
+func TestRandomPort(t *testing.T) {
+	p, err := RandomPort()
+	assert.Nil(t, err)
+	assert.Equal(t, true, p > 0)
+}
